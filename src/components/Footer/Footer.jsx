52f35a80923319f973ff36831d91bac098a64b91
Kishkin Vitalii <dimpla30@gmail.com>
2023-03-10 13:31:46 +0300

    create Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+import styles from "../../styles/Footer.module.css";
+import { ROUTES } from "../../utils/routes";
 
+import Logo from "../../images/logo.svg";
+
+const Footer = () => (
+  <section className={styles.footer}>
+    <div className={styles.logo}>
+      <Link to={ROUTES.HOME}>
+        <img src={Logo} alt="Stuff" />
+      </Link>
+    </div>
+
+    <div className={styles.rights}>
+      Developed by{" "}
+      <a href="https://github.com/ZimmerF0" target="_blank" rel="noreferrer">
+        Kishkin Vitalii
+      </a>
+    </div>
+
+    <div className={styles.socials}>
+      <a href="https://instagram.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+        </svg>
+      </a>
+
+      <a href="https://facebook.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+        </svg>
+      </a>
+
+      <a href="https://youtube.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+        </svg>
+      </a>
+    </div>
+  </section>
+);
 export default Footer;
