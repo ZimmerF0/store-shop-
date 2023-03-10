b410c40e6c63d1501195234a5169f7ab400d5dc4
Kishkin Vitalii <dimpla30@gmail.com>
2023-03-10 17:06:26 +0300

    create Products component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Poster from "../Poster/Poster";
+import Products from "../Products/Products";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { list } = useSelector(({ products }) => products);
+
+  return (
+    <>
+      <Poster />
+      <Products products={list} amount={5} title="Trending" />
+    </>
+  );
 };
 
 export default Home;
