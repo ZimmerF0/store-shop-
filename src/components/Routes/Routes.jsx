23a1fd7c0fe97dd632618740696b703f6cdf2b03
Kishkin Vitalii <dimpla30@gmail.com>
2023-03-13 21:37:49 +0300

    creating Product page

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-// import { ROUTES } from "../../utils/routes";
+import { ROUTES } from "../../utils/routes";
 
 import Home from "../Home/Home";
+import SingleProducts from "../Products/SingleProducts";
 
 const AppRoutes = () => (
   <Routes>
     <Route index element={<Home />} />
+    <Route path={ROUTES.PRODUCT} element={<SingleProducts />} />
   </Routes>
 );
 
