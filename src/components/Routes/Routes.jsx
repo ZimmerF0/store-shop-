74183ae2c9661fc59f738c0edd9ffdeb3950378e
Kishkin Vitalii <dimpla30@gmail.com>
2023-03-16 14:46:36 +0300

    modifyed Category component

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -6,12 +6,14 @@ import { ROUTES } from "../../utils/routes";
 import Home from "../Home/Home";
 import SingleProducts from "../Products/SingleProducts";
 import Profile from "../Profile/Profile";
+import SingleCategory from "../Categories/SingleCategory";
 
 const AppRoutes = () => (
   <Routes>
     <Route index element={<Home />} />
     <Route path={ROUTES.PRODUCT} element={<SingleProducts />} />
     <Route path={ROUTES.PROFILE} element={<Profile />} />
+    <Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
   </Routes>
 );
 
