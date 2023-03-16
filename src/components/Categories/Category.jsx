af8011bc2ddc3056ca38e9ab6a07ec70952fc269
Kishkin Vitalii <dimpla30@gmail.com>
2023-03-16 15:55:35 +0300

    create Cart component

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -70,6 +70,12 @@ const Category = () => {
     setParams({ ...defaultParams, ...values });
   };
 
+  const handleReset = () => {
+    setValues(defaultValues);
+    setParams(defaultParams);
+    setEnd(false);
+  };
+
   return (
     <section className={styles.wrapper}>
       <h2 className={styles.title}>{cat?.name}</h2>
@@ -113,7 +119,7 @@ const Category = () => {
       ) : !isSuccess || !items.length ? (
         <div className={styles.back}>
           <span>No results</span>
-          <button>Reset</button>
+          <button onClick={handleReset}>Reset</button>
         </div>
       ) : (
         <Products
